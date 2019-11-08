7a49082999fe363b611db0bebdb25e48a03cb4cf
jobi <olajobinurudeen2013@gmail.com>
2019-11-08 01:20:53 +0100

    adding checkbox and event to todo

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Todoitem from './Todoitem';
+import PropTypes from 'prop-types';
 import '../App.css';
 
 class Todos extends Component {
@@ -6,9 +8,15 @@ class Todos extends Component {
   render() {
    
     return this.props.todos.map((todo) => (
-      <h3>{ todo.title }</h3>
+     <Todoitem key={todo.id} todo={todo} markComplete = {this.props.markComplete} />
     ))
   }
+}
+
+
+// eslint-disable-next-line react/no-typos
+Todos.PropTypes = {
+  todos: PropTypes.array.isRequired
 }
 
 export default Todos;
